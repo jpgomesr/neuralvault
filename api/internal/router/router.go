5241f01b044c866df83ba9bc5caaef58f55b90d8
Joao Paulo <147559751+jpgomesr@users.noreply.github.com>
2026-06-03 19:39:27 -0300

    chore(router): update NewRouter to accept config and add Swagger routes

--- api/internal/router/router.go
+++ api/internal/router/router.go
@@ -2,16 +2,19 @@ package router
 
 import (
 	"github.com/go-chi/chi/v5"
+	_ "github.com/jpgomesr/NeuralVault/docs"
+	"github.com/jpgomesr/NeuralVault/internal/config"
 	"github.com/jpgomesr/NeuralVault/internal/health"
+	httpSwagger "github.com/swaggo/http-swagger"
 )
 
-func NewRouter() *chi.Mux {
+func NewRouter(cfg *config.Config) *chi.Mux {
 	r := chi.NewRouter()
 
 	// Example of how to add a new service and handler:
 	// exampleService := example.ExampleService{}
 	// exampleHandler := example.NewHandler(exampleService)
-	
+
 	healthService := health.HealthService{}
 	healthHandler := health.NewHandler(healthService)
 
@@ -19,6 +22,9 @@ func NewRouter() *chi.Mux {
 		// Example of how to mount a new service's routes:
 		// r.Mount("/example", example.Routes(exampleHandler))
 		r.Mount("/health", health.Routes(healthHandler))
+
+		// Swagger routes
+		r.Get("/swagger/*", httpSwagger.WrapHandler)
 	})
 
 	return r
